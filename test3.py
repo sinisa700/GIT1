ff797655b933839ba2597a22b3f90a1ae7c6b004
sinisa <sinisa.dukic@yahoo.com>
2025-01-13 20:08:48 +0100

    dodan repo url check

--- test3.py
+++ test3.py
@@ -7,3 +7,15 @@ print("This is a code cell")
 
 # %%
 print("This is another code cell")
+
+
+# %%
+import subprocess
+
+# Get the URL of the GitHub repository
+def get_git_repo_url():
+    result = subprocess.run(['git', 'config', '--get', 'remote.origin.url'], stdout=subprocess.PIPE, text=True)
+    print('ovo je print: '+str(result))
+    return result.stdout.strip() 
+get_git_repo_url()
+# %%
